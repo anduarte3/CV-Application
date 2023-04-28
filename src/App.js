2b578ac242b65afb36731598f9c41403e18aefe9
FADEwt <diamonvhero@gmail.com>
2023-04-28 19:01:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//import Overview from "./components/Overview"; 
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return <div>
+            
+        </div>
+    }
 }
 
 export default App;
+
+
+//CV APP consists of:
+
+//Personal Details 
+
+//name 
+// role
+// number*
+// mail*
+// city*
+// intro
+
+//Work experience
+
+//job location
+//job role
+//date join
+//date end (or present)
+
+//Education
+
+
+//School
+//degree curso tecnologico
+//subject formacao tecnica em eletroncia e computadores
+//Date join
+//Date end
+
+
+//Things to consider
+
+//Button to add delete to add more work experiences or education studies
+//Perhaps add small logo 
+
+//Every section (Personal, Work, Education, should have add and delete button to submit or go back in the CV)
+
+
+//What is react?
+// Basically instead of vanilla javascript using the DOM,
+// Instead we create components as in small parts an let React interact
+//Tree of component
+
+//Components are basically a functgion that returns the HTML through JSX
+
+//Example App.js -> add some data, then in compoennt function pass props argument and call it props.text
+//If it changes in App.js it will also change for react component
